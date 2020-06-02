28377363e1fb6aa6481b46400835ed05c25e2ad3
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-06-02 11:37:51 +0530

    Add Popup script

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// This script opens a popup and display the content
+// When click/tap on the game object
+public class Popup : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
