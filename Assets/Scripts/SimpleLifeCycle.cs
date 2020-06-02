bd4702ee60d57b22731564bff1a14e2b318114ba
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-06-02 11:44:16 +0530

    Add SimpleLifeCycle script

---
+++ Assets/Scripts/SimpleLifeCycle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// This script shows the simple life cycle of the animal
+// This script will controll the Arrows
+public class SimpleLifeCycle : MonoBehaviour
+{
+    public GameObject[] Arrows;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
