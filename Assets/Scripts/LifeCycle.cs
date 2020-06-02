d3cf8e9e1eb9a09aff9619515bbc6587a1e463c2
Vamshi Krishna <matamvamshikrishna@gmail.com>
2020-06-02 11:44:34 +0530

    Add LifeCylce script

---
+++ Assets/Scripts/LifeCycle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// This script shows the complete lifecycle of
+// the animal step by step
+public class LifeCycle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
