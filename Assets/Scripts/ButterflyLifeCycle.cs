9ecd49b7ae5fa30ab0cbefe176ca56305ab95232
tan117 <65021192+tan117@users.noreply.github.com>
2020-06-05 11:10:04 +0530

    Add ButterflyLifeCycle

---
+++ Assets/Scripts/ButterflyLifeCycle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine.UI;
+
+
+// This script shows the complete lifecycle of
+// the animal step by step
+
+public class ButterflyLifeCycle : MonoBehaviour
+{
+
+ 
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("animation1"))
+        {
+        
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("animation2"))
+        {
+        
+         
+
+
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("animation3"))
+        {
+           
+        }
+    }
+
+
+}
