67169fc9be66032489b62e00a656b0fea89724fc
AtulSingh2000 <atulkumar102s@gmail.com>
2020-06-29 16:09:42 +0530

    segregated scenes based on the cycle stages

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,13 +6,13 @@ public class MainMenu : MonoBehaviour
     public void On3stageButtonClick()
     {
         Stage.type = Stage.Type.Stage_3;
-        SceneManager.LoadScene("AR");
+        SceneManager.LoadScene("3Stage");
     }
 
     public void On4stageButtonClick()
     {
         Stage.type = Stage.Type.Stage_4;
-        SceneManager.LoadScene("AR");
+        SceneManager.LoadScene("4Stage");
     }
     
     public void OnExitButtonClick()
